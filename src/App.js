4bf366c8b0ba1f03b4ad1e1382dde6e7ff6533a2
Mohammad <MohammadAlzeer400@gmail.com>
2023-05-27 14:11:30 -0700

    public project for deploy

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>Hi this is Mohammad And this is My first app to dep;oy</div>
+    </div>
+  );
+}
+
+export default App;
